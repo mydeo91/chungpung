7c97c27ed4fc4e79d4b173c1ef8f87479892600c
mydeo91 <leh2030@icloud.com>
2019-02-21 15:17:53 +0900

    gh-pages test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter basename="/chungpung">
+    <BrowserRouter>
         <App />
     </BrowserRouter>, 
     document.getElementById('contentWrapper')
