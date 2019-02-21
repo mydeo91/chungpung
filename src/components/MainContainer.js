dedad7490d86202d1e992c66f0c33c29d2267079
mydeo91 <leh2030@icloud.com>
2019-02-21 14:59:31 +0900

    gh-pages test

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -15,7 +15,7 @@ export class MainContainer extends Component {
         <div className={styles.contentbox}>
           <Switch>
             <Route exact path="/" component={Main} />
-            <Route exact path="/pizza" component={Pizza} />
+            <Route exact path="/chungpung/pizza" component={Pizza} />
             {/* <Route exact path="/guestHouse" component={Main} />
             <Route exact path="/strongFire" component={Main} />
             <Route exact path="/service" component={Main} />
