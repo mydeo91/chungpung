ef00f07ca35fac96272c2cd6037f488aee3bd39f
mydeo91 <leh2030@icloud.com>
2019-05-16 03:05:09 +0900

    Updates

---
+++ precache-manifest.8a7352174ff1cc6f3b1a6f9f6ed807a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76e187d9c9656976f86c3e9d6fc0cd9",
+    "url": "/chungpung/index.html"
+  },
+  {
+    "revision": "a4dc5f37bb2639327e69",
+    "url": "/chungpung/static/css/main.f53b35de.chunk.css"
+  },
+  {
+    "revision": "fdc0c66cc0f81d52e1ea",
+    "url": "/chungpung/static/js/2.0ff957b0.chunk.js"
+  },
+  {
+    "revision": "a4dc5f37bb2639327e69",
+    "url": "/chungpung/static/js/main.5fb97c7e.chunk.js"
+  },
+  {
+    "revision": "4ce425c526c76f87e8a1",
+    "url": "/chungpung/static/js/runtime~main.db8ffab9.js"
+  },
+  {
+    "revision": "2735694f178ea8a7dd495795ec58cf6b",
+    "url": "/chungpung/static/media/bg-guesthouse.2735694f.jpg"
+  },
+  {
+    "revision": "98f4190362bdb1104e6b9c87e6b918b4",
+    "url": "/chungpung/static/media/bg-strong.98f41903.jpg"
+  },
+  {
+    "revision": "6e6b892eb87a204598423c0a84fee59f",
+    "url": "/chungpung/static/media/room01.6e6b892e.jpg"
+  }
+]);
